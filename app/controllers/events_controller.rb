7be5493245421a9b6b4e7e3e9da0e9507c0e719f
chrisaped <chrisaped@gmail.com>
2013-07-21 12:34:56 -0400

    added events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,17 @@
 class EventsController < ApplicationController
   def new
+  	@event = Event.new
   end
 
   def edit
+  end
+
+  def create
+  	@event = Event.create(params[:event])
+  	redirect_to root_url
+  end
+
+  def show
+  	@event = Event.find(params[:id])
   end
 end
